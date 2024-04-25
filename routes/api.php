8304deb84e4a3f10e7b152e05fbcbfdbd8e5e35e
MadiyarZh <mzhorakhan00@gmail.com>
2024-04-25 13:35:57 +0500

    Task! CRUD Rest-api Laravel 10 version

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/product/{id}', [ProductController::class, 'show']);
-Route::post('/product/create', [ProductController::class, 'store']);
-Route::patch('/product/update/{id}', [ProductController::class, 'update']);
-Route::get('/product/brand/{name}', [ProductController::class, 'getByBrand']);
